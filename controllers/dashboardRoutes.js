391288ffe9725417d350b9efc066016b3ad291af
Zach Berger <zachhberger@gmail.com>
2023-05-31 19:37:43 -0400

    dashboard routes added

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    // render dashboard with user's blog posts
+router.get('/', isLoggedIn, (req, res) => {
+  BlogPost.findAll({
+      where: {
+          user_id: req.session.user.id
+      }
+  })
+  .then(blogPosts => {
+      res.render('dashboard', { blogPosts });
+  })
+  .catch(err => {
+      res.status(500).json({ message: 'Error retrieving blog posts' });
   });
+});
   
-  router.delete('/:id', (req, res) => {
-    // delete a blog post
+router.delete('/:id', isLoggedIn, (req, res) => {
+  BlogPost.destroy({
+      where: {
+          id: req.params.id,
+          user_id: req.session.user.id
+      }
+  })
+  .then(deleted => {
+      if (deleted) {
+          res.status(200).json({ message: 'Blog post deleted successfully' });
+      } else {
+          res.status(404).json({ message: 'Blog post not found' });
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ message: 'Error deleting blog post' });
   });
+});
 
-  module.exports = router;
+module.exports = router;
