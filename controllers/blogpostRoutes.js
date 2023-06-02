eedcafb032756f0638a740b65208ad8964a69778
Zach Berger <zachhberger@gmail.com>
2023-06-01 21:52:14 -0400

    fixed blog post content loading

--- controllers/blogpostRoutes.js
+++ controllers/blogpostRoutes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const { Blogpost } = require('../models');
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Blogpost.findById(id)
-      .then(post => {
-        res.render('blogpost', post);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-      });
-  });
+  const { id } = req.params;
+    Blogpost.findByPk(id)
+    .then(blogPost => {
+      res.render('blogpost', { blogPost });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
   
 router.get('/post/:id', (req, res) => {
   Blogpost.findByPk(req.params.id, {
