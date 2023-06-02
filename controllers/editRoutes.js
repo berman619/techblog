2c777044876b642fb91bdbca957f644faf43dfc6
Zach Berger <zachhberger@gmail.com>
2023-06-01 21:30:15 -0400

    fixed route for creating new post vs editng post

--- controllers/editRoutes.js
+++ controllers/editRoutes.js
@@ -27,39 +27,39 @@ router.get('/:id?', isLoggedIn, (req, res) => {
 });
 
 router.post('/', isLoggedIn, (req, res) => {
-    Blogpost.create({
-        title: req.body.title,
-        content: req.body.content,
-        user_id: req.session.user.id
-    })
-    .then(blogPost => {
-        res.redirect('/dashboard');
-    })
-    .catch(err => {
-        res.status(500).json({ message: 'Error creating blog post' });
-    });
+  Blogpost.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user.id
+  })
+  .then(blogPost => {
+      res.status(200).json({ message: 'Blog post created successfully' });
+  })
+  .catch(err => {
+      res.status(500).json({ message: 'Error creating blog post' });
+  });
 });
 
 router.put('/:id', isLoggedIn, (req, res) => {
-    Blogpost.update({
-        title: req.body.title,
-        content: req.body.content,
-    }, {
-        where: {
-            id: req.params.id,
-            user_id: req.session.user.id
-        }
-    })
-    .then(updated => {
-        if (updated[0] > 0) {
-            res.status(200).json({ message: 'Blog post updated successfully' });
-        } else {
-            res.status(404).json({ message: 'Blog post not found' });
-        }
-    })
-    .catch(err => {
-        res.status(500).json({ message: 'Error updating blog post' });
-    });
+  Blogpost.update({
+      title: req.body.title,
+      content: req.body.content,
+  }, {
+      where: {
+          id: req.params.id,
+          user_id: req.session.user.id
+      }
+  })
+  .then(updated => {
+      if (updated[0] > 0) {
+          res.status(200).json({ message: 'Blog post updated successfully' });
+      } else {
+          res.status(404).json({ message: 'Blog post not found' });
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ message: 'Error updating blog post' });
+  });
 });
 
 module.exports = router;
